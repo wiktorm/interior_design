ade026111e2c5ad1a574c019beae76b8bdb579d0
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-07-03 22:46:41 +0100

    sgallery.php - first step done.
    sgallery runs, still long way to go - tpls and css.

---
+++ sgallery.php
@@ -0,0 +1,11 @@
+<?php
+// error_reporting(0);
+$loader = include __DIR__ . '/vendor/autoload.php';
+
+use Hoborg\SGallery\Application;
+
+$console = new Application();
+var_dump('test');
+$console->setApplicationRoot(__DIR__ . '/vendor/hoborglabs/sgallery');
+$console->setConfigurationOverride(__DIR__ . '/sgallery.properties');
+$console->run();
