9ad351bf5442957549856d3ae1d059474dd54c2e
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-07-04 21:15:15 +0100

    [no-ref] Added sGallery templates and updated sgallery.php
    Now the only thinkg to do is to update skin for sgallery.

--- sgallery.php
+++ sgallery.php
@@ -8,4 +8,5 @@ $console = new Application();
 var_dump('test');
 $console->setApplicationRoot(__DIR__ . '/vendor/hoborglabs/sgallery');
 $console->setConfigurationOverride(__DIR__ . '/sgallery.properties');
+$console->addExtensionPath(__DIR__ . '/sgallery');
 $console->run();
