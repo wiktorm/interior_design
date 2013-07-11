3d22f03324f1a92ab57cf501c65a799ec9bf2e16
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-07-11 14:15:44 +0100

    [refs #2] Adding description files to gallery folders
    * updated sgallery min version to 0.3.1
    * removed `var_dump`  from sgallery.php
    * updating CSS and tpls for gallery

--- sgallery.php
+++ sgallery.php
@@ -5,7 +5,6 @@ $loader = include __DIR__ . '/vendor/autoload.php';
 use Hoborg\SGallery\Application;
 
 $console = new Application();
-var_dump('test');
 $console->setApplicationRoot(__DIR__ . '/vendor/hoborglabs/sgallery');
 $console->setConfigurationOverride(__DIR__ . '/sgallery.properties');
 $console->addExtensionPath(__DIR__ . '/sgallery');
